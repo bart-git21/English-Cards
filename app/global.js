4d764e24b3025650512af5e8529618ab5abadc0d
gitName <bartk@list.ru>
2022-11-25 06:48:46 +0300

    add global functions file

---
+++ app/global.js
@@ -0,0 +1,14 @@
+function shuffle(arr) {
+    for (let i = arr.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        [arr[i], arr[j]] = [arr[j], arr[i]];
+    };
+}
+
+const shuffleBtn = document.querySelector(".english__shuffle");
+
+
+export {
+    shuffle,
+    shuffleBtn,
+};
