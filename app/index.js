d8535b0d364ec902fc77fc3ce7eb805f7bea23cb
gitName <bartk@list.ru>
2022-11-26 20:26:40 +0300

    refactor: add start function

--- app/index.js
+++ app/index.js
@@ -1,10 +1,19 @@
 import {visibleBactToTopButton, 
     backToTop
 } from "./backtotop.js";
+import {newDesk} from "./sidebarButtons.js";
 import {addClickListener} from "./script.js";
 
 
 
 document.addEventListener("scroll", visibleBactToTopButton);
 backToTop();
-addClickListener();
+async function start() {
+    await new Promise(
+        res => {
+            setTimeout(()=>{res(newDesk.createButtons())}, 100);
+        }
+    );
+    addClickListener();
+}
+start();
